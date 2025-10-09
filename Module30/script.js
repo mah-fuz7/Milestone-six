575fd6122887ee1a07b71c5addc506b72671a1e6
mah-fuz7 <mahafuz846@gmail.com>
2025-10-09 11:26:26 +0600

    module31

--- Module30/script.js
+++ Module30/script.js
@@ -54,3 +54,65 @@ const students=driver.map((num,index)=> {
 
 // .forEach() → no return needed, because it’s only used for side effects (like logging).
 
+
+//1. map
+
+let nunbers=[2,5,8]
+let tripleNum=number.map(num=>num*3)
+// console.log(tripleNum)
+
+// If you don’t return a value inside the callback, the new array will have undefined values
+
+let tripleNum2=number.map(num=>{
+    num*3
+    return num*3
+})
+// when you have only one line of code you can skip the return and {} bracket
+//  but if you have more than one line of code you have to use return and {} bracket
+//  otherwise it will return undefined
+
+// console.log(tripleNum2)
+
+
+let schoolStudents=[
+    {id:1,name:'karim', age:15},
+    {id:2,name:'rahim', age:16},
+    {id:3,name:'halim', age:17},
+    {id:4,name:'salim', age:18},
+    {id:5,name:'jalim', age:19},
+];
+   
+
+let AdultStudents=schoolStudents.map(obj=>obj.name)
+// console.log(AdultStudents)
+
+// 2. forEach ==> You use forEach() when you want to loop through an array and do something with each element, but you don’t need a new array as a result.
+
+let evenIds=schoolStudents.forEach(student=>student.id%2===0)
+
+// console.log(evenIds) // it will return undefined because forEach does not return anything .that's means you cannot store the result of forEach in a variable .it works like a loop only
+
+
+// correct way to use forEach
+schoolStudents.forEach(student=>{
+    // console.log(student.id*2)
+})
+
+// filter
+
+let primeNumbers=[2,3,5,7,11,123,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,88,97]
+
+let TwoDivided=primeNumbers.filter(num=>num%2===0);
+// console.log(TwoDivided)
+
+// find
+let firstEven=primeNumbers.find(num=>num%2===0)
+console.log(firstEven)
+
+// find vs filter
+
+// filter returns all the matching elements in an array
+
+// find returns the first matching element in an array and not returns an array
+
+// ------------------ 30-5--------------------------
---
+++ Module31/script.js
@@ -0,0 +1,44 @@
+// ------------------ 31-1-------------------------
+//question=> js is a dynamically typed language
+//question=> what is the difference between primitive and non-primitive data types in js
+
+// ------------------ 31-2-------------------------
+
+// difference between null and undefined
+
+// ------------------ 33-3-------------------------
+
+// truthy and falsy values in js
+// double not operator and single not operator
+
+// ------------------ 33-4-------------------------
+
+// triple equal operator and double equal operator
+
+// ------------------ 33-5------------------------- 
+
+// scope in js
+// block scope, function scope, global scope
+
+// ------------------ 33-6-------------------------
+
+// callback function
+
+// ------------------ 33-7-------------------------
+
+// pass by values and pass by reference
+// arrgumnets and array like object
+
+// ------------------ 33-8-------------------------
+// what is closure in js skip
+
+// ------------------ 33-9-------------------------
+
+// post and pre increment and decrement operator
+
+// ------------------ 33-10-------------------------
+
+// hosting in js
+// what is temporal dead zone in js
+
+// this module is based on js interview questions 
